0ab16512578eb837ae0fe76d97ebce2a8fab3df4
Sandeep M V <sandeepmvd18@outlook.com>
2024-03-14 20:45:47 +0530

    Update sms_marketing.py

--- sms_marketing.py
+++ sms_marketing.py
@@ -1,24 +1,19 @@
 from twilio.rest import Client
 
-# Twilio credentials
 account_sid = '{accountsid_no}'
 auth_token = '{token_num}'
 twilio_number = '+{generated_num}'
 
-# Initialize Twilio client
 client = Client(account_sid, auth_token)
 
-# Message to be sent
 message_body = """Dear Customer,
 
 Discover a world of exceptional service at Sri Vijaya Motors, located at Bethamangala Rd, Kolar Gold Fields, Karnataka 563116, India. From top-notch maintenance to expert guidance, we're here to elevate your experience. Visit us today to explore our range of services and products.
 
 - Sri Vijaya Motors"""
 
-# List of numbers to send messages to
 numbers = ['number of our choice']  # Add your numbers here
 
-# Send messages
 for number in numbers:
     message = client.messages.create(
         body=message_body,
